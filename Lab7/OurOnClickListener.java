b04fbe478d9be7fea666fb6d55b1ac5499253997
laksri <laksri@gmail.com>
2015-09-02 04:25:52 -0400

    Lab 1 and 7
    Rest to come soon

---
+++ Lab7/OurOnClickListener.java
@@ -0,0 +1,25 @@
+package com.example.helloworld;
+
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class OurOnClickListener implements OnClickListener {
+
+    MainActivity caller;
+    private int count;
+
+    public OurOnClickListener(MainActivity activity) {
+        this.caller = activity;
+        this.count = 0;
+    }
+
+    @Override
+    public void onClick(View v) {
+        count++;
+        String countstr = Integer.toString(count);
+        caller.textView.setText("Clicked " + countstr + " times");
+
+    }
+
+}
